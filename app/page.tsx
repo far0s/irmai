7a078f8d3626746b51b597be1c6edd949ff33201
Seb Dancer-Michel <s.dancermichel@gmail.com>
2023-11-17 18:57:04 +0100

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,35 @@
 'use client';
 
+import { useEffect } from 'react';
 import { useChat } from 'ai/react';
+import Recorder from '@/components/recorder/Recorder';
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit, data } = useChat();
+
+  // PRIO 1
+  // [] speech recorder so people can talk directly to their browser (using browser speech recognition API https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition)
+  // [] speech synthesizer so the browser can talk back (using OpenAI text-to-speech API https://platform.openai.com/docs/guides/text-to-speech)
+  // PRIO 2
+  // [] custom instructions (the AI should be proficient with astrology, etc.)
+  // [] pull tarot cards from https://github.com/ekelen/tarot-api and send them to the AI
+
+  useEffect(() => {
+  if (messages.length > 0) {
+    const lastMessage = messages[messages.length - 1];
+    console.log(lastMessage);
+    if (lastMessage.role === 'assistant') {
+      // speakText(lastMessage.content);
+    }
+  }
+}, [messages]);
+
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       {messages.length > 0
         ? messages.map(m => (
             <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === 'user' ? 'User: ' : 'AI: '}
+              {m.role === 'user' ? 'User: ' : 'IrmAI: '}
               {m.content}
             </div>
           ))
@@ -23,6 +43,7 @@ export default function Chat() {
           onChange={handleInputChange}
         />
       </form>
+      <Recorder />
     </div>
   );
 }
---
+++ components/recorder/Recorder.tsx
@@ -0,0 +1,70 @@
+// 'use client';
+import { useState, useEffect } from 'react';
+
+declare const window: any;
+
+const Recorder = () => {
+  const [isRecording, setIsRecording] = useState(false);
+  const [confidence, setConfidence] = useState<number>();
+  const [transcript, setTranscript] = useState<number>();
+  const [recognition, setRecognition] = useState(null);
+
+  useEffect(() => {
+    return () => {
+      const speechRecognitionList = (window as any).SpeechGrammarList || (window as any).webkitSpeechGrammarList;
+      const recog = window.SpeechRecognition || window.webkitSpeechRecognition;
+      const recognitionInstance = new recog();
+      const list = new speechRecognitionList();
+      recognitionInstance.grammars = list;
+      recognitionInstance.lang = 'en-US';
+      recognitionInstance.interimResults = true;
+      recognitionInstance.continuous = true;
+      recognitionInstance.maxAlternatives = 1;
+
+      recognitionInstance.onstart = function() {
+        setIsRecording(true);
+        console.log("We are listening. Try speaking into the microphone.");
+      };
+
+      recognitionInstance.onend = function() {
+        // when user is done speaking
+        setIsRecording(false);
+        recognitionInstance.stop();
+      }
+
+      recognitionInstance.onresult = function(event: any) {
+        setTranscript(event.results[0][0].transcript);
+        console.log('Transcript: ' + event.results[0][0].transcript);
+        setConfidence(event.results[0][0].confidence);
+        console.log('Confidence: ' + event.results[0][0].confidence);
+      };
+
+      recognitionInstance.onerror = function(event:any) {
+        console.log(event.error);
+        setIsRecording(false);
+      }
+
+      setRecognition(recognitionInstance);
+
+    };
+  }, [])
+
+  const startRecording = () => {
+    console.log('record');
+    recognition.start();
+  };
+
+  const stopRecording = () => {
+    console.log('stop record');
+    recognition.stop();
+  }
+
+  return (
+    <div className="fixed bottom-0 w-full max-w-md p-2 mb-20">
+      <button className="border border-gray-300 rounded p-2 hover:bg-gray-100" onClick={startRecording}>Record{isRecording ? 'ing...' : ''}</button>
+      {isRecording && <button className="border border-gray-300 rounded p-2 hover:bg-gray-100" onClick={stopRecording}>Stop</button>}
+    </div>
+  )
+};
+
+export default Recorder;
