cf24fcc9c39c8b4f08de52568e4c6d6bcd6e3d32
Seb Dancer-Michel <s.dancermichel@gmail.com>
2024-04-22 00:45:56 +0200

    chore: move CTA to footer

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,9 @@ import Stage from "@/components/Stage/Stage";
 import StageScreens from "@/components/Stage/Stage.utils";
 import Footer from "@/components/Footer/Footer";
 import Debug from "@/components/Debug/Debug";
-import PressAndHoldCTA from "@/components/PressAndHoldCTA/PressAndHoldCTA";
 
 const Home = () => {
-  const { globalState, setGlobalState } = useIrmaiStore((s) => s);
+  const { globalState } = useIrmaiStore((s) => s);
 
   return (
     <main className={s.page}>
@@ -18,25 +17,6 @@ const Home = () => {
         {Object.entries(StageScreens).map(([key, Component]) => (
           <Component key={key} isActive={globalState === key} />
         ))}
-
-        <div>
-          {Object.entries(StageScreens).map(
-            ([key], i) =>
-              globalState === key && (
-                <PressAndHoldCTA
-                  key={key}
-                  onBeginPress={() => {
-                    console.log("onBeginPress", key);
-                  }}
-                  onEndPress={() => {
-                    const keys = Object.keys(StageScreens);
-                    const nextKey: any = keys[i + 1];
-                    setGlobalState(nextKey || "splash");
-                  }}
-                />
-              )
-          )}
-        </div>
       </Stage>
       <Footer />
       <Debug />
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,16 +1,38 @@
 import Link from "next/link";
 import s from "./footer.module.css";
+import { useIrmaiStore } from "@/components/ZustandStoreProvider/ZustandStoreProvider";
+import StageScreens from "@/components/Stage/Stage.utils";
+import PressAndHoldCTA from "@/components/PressAndHoldCTA/PressAndHoldCTA";
 
 const Footer = () => {
+  const { globalState, setGlobalState } = useIrmaiStore((s) => s);
+
   return (
     <footer className={s.footer}>
       {/* insert actions here */}
       {/* Press and hold CTA */}
       {/* Cards thumbnails */}
       {/* Shortlinks ("Learn more, End conversation") */}
-      <Link href="#" className={s.link}>
+      {Object.entries(StageScreens).map(
+        ([key], i) =>
+          globalState === key && (
+            <PressAndHoldCTA
+              key={key}
+              onBeginPress={() => {
+                console.log("onBeginPress", key);
+              }}
+              onEndPress={() => {
+                const keys = Object.keys(StageScreens);
+                const nextKey: any = keys[i + 1];
+                setGlobalState(nextKey || "splash");
+              }}
+            />
+          )
+      )}
+
+      {/* <Link href="#" className={s.link}>
         Learn more
-      </Link>
+      </Link> */}
     </footer>
   );
 };
