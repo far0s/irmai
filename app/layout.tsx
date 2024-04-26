e5a31c1f6073f44f8118eea9c590fe72011c0e6a
Seb Dancer-Michel <s.dancermichel@gmail.com>
2024-04-26 11:40:10 +0200

    chore: update deps, move fonts to next/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { StoreProvider } from "@/components/ZustandStoreProvider/ZustandStoreProvider";
 import { type Viewport, Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
+import localFont from "next/font/local";
 
 export function generateMetadata(): Metadata {
   return {
@@ -28,13 +29,62 @@ export function generateViewport(): Viewport {
   };
 }
 
+const objectSans = localFont({
+  src: [
+    {
+      path: "../public/fonts/subset-PPObjectSans-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/subset-PPObjectSans-Slanted.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/subset-PPObjectSans-Heavy.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/subset-PPObjectSans-HeavySlanted.woff2",
+      weight: "800",
+      style: "italic",
+    },
+  ],
+  display: "swap",
+  variable: "--font-object-sans",
+});
+
+const cirka = localFont({
+  src: [
+    {
+      path: "../public/fonts/PPCirka.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/subset-PPCirka-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/subset-PPCirka-Bold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  variable: "--font-cirka",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${objectSans.className} ${cirka.className}`}>
       <head>
         <link rel="icon" href="/images/irmai-favicon.png" sizes="any" />
         <link rel="apple-touch-icon" href="/images/irmai-favicon.png" />
