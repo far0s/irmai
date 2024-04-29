ba8f98cd08ce3365024dfd206024cb70fd4498cf
Seb Dancer-Michel <s.dancermichel@gmail.com>
2024-04-29 16:28:00 +0200

    chore: update test text with irmai to test pronunciation

--- app/api/text-to-speech/route.ts
+++ app/api/text-to-speech/route.ts
@@ -3,14 +3,14 @@ import OpenAI from "openai";
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY || '',
+  apiKey: process.env.OPENAI_API_KEY || "",
 });
 
 // IMPORTANT! Set the runtime to edge
-export const runtime = 'edge';
+export const runtime = "edge";
 
 export async function POST(req: Request) {
-  const { input, voice = 'nova' } = await req.json();
+  const { input, voice = "nova" } = await req.json();
 
   const mp3 = await openai.audio.speech.create({
     model: "tts-1",
@@ -33,7 +33,8 @@ export async function GET(req: Request) {
   const mp3 = await openai.audio.speech.create({
     model: "tts-1",
     voice: "nova",
-    input: "Hi!, I'm Nova, and this is a test message. Beep! Sorry, I'm not a message machine. I'm a robot. I mean no, I'm not a robot, I'm an AI assistant. Anyway, talk to you soon!",
+    input:
+      "Hi!, I'm irmai, and this is a test message. Beep! Sorry, I'm not a message machine. I'm a robot. I mean no, I'm not a robot, I'm an AI assistant. Anyway, talk to you soon!",
   });
 
   const buffer = await Buffer.from(await mp3.arrayBuffer());
