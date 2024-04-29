0aff8cdb6ae831065fc1bc68a51ebc5b69da63a1
Seb Dancer-Michel <s.dancermichel@gmail.com>
2024-04-29 19:01:30 +0200

    Revert "chore: move fonts in utils"
    This reverts commit 6ccd16cf73a73182bfb3fe4589010e71fb21f087.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { StoreProvider } from "@/components/ZustandStoreProvider/ZustandStoreProvider";
 import { type Viewport, Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
-import { objectSans, cirka } from "@/utils/fonts";
+import localFont from "next/font/local";
 
 export function generateMetadata(): Metadata {
   return {
@@ -25,9 +25,58 @@ export function generateViewport(): Viewport {
     initialScale: 1,
     userScalable: false,
     themeColor: "black",
-    colorScheme: "dark light",
+    colorScheme: "dark",
   };
 }
+
+const objectSans = localFont({
+  src: [
+    {
+      path: "../public/fonts/subset-PPObjectSans-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/subset-PPObjectSans-Slanted.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/subset-PPObjectSans-Heavy.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/subset-PPObjectSans-HeavySlanted.woff2",
+      weight: "800",
+      style: "italic",
+    },
+  ],
+  display: "swap",
+  variable: "--font-object-sans",
+});
+
+const cirka = localFont({
+  src: [
+    {
+      path: "../public/fonts/PPCirka.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/subset-PPCirka-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/subset-PPCirka-Bold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  variable: "--font-cirka",
+});
 
 export default function RootLayout({
   children,
