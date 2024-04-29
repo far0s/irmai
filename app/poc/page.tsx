ad7b770cb7443ac1daab85221eff724889918d0f
Seb Dancer-Michel <s.dancermichel@gmail.com>
2024-04-29 18:16:51 +0200

    chore: move poc component to _poc

--- app/poc/page.tsx
+++ app/poc/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useChat } from "ai/react";
-import Recorder from "../../components/recorder/Recorder";
+import Recorder from "@/components/_poc/recorder/Recorder";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit, data } = useChat();
