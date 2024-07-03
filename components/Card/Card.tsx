922ca3bee111563e252c753bced145543ce4d34c
Seb Dancer-Michel <s.dancermichel@gmail.com>
2024-07-03 16:40:39 +0200

    feat<Card>: improve lightbox

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -66,43 +66,45 @@ const Card = ({
         </TransitionWrapper>
       </div>
 
-      {lightboxOpen && (
-        <div className={s.lightbox} onClick={() => setLightboxOpen(false)}>
-          <div className={s.lightboxContent}>
-            <div
-              className={s.card}
-              data-is-hidden={false}
-              data-variant="lightbox"
-            >
-              <div className={s.cardBack}>
-                <Logo />
-                <Logo />
-              </div>
-              <div className={s.cardFront}>
-                {card.image && (
-                  <Image
-                    src={card.image}
-                    alt={card.name}
-                    width={300}
-                    height={527}
-                    className={s.cardImage}
-                    priority
-                  />
-                )}
-              </div>
-
-              <TransitionWrapper
-                className={s.cardName}
-                show={!hidden}
-                delay={200}
-              >
-                <span>{card.name}</span>
-                {reverse === true && <span>(reverse)</span>}
-              </TransitionWrapper>
+      <div
+        className={s.lightbox}
+        data-show={lightboxOpen}
+        onClick={() => setLightboxOpen(false)}
+      >
+        <div className={s.lightboxContent}>
+          <div
+            className={s.card}
+            data-is-hidden={false}
+            data-variant="lightbox"
+          >
+            <div className={s.cardBack}>
+              <Logo />
+              <Logo />
             </div>
+            <div className={s.cardFront}>
+              {card.image && (
+                <Image
+                  src={card.image}
+                  alt={card.name}
+                  width={300}
+                  height={527}
+                  className={s.cardImage}
+                  priority
+                />
+              )}
+            </div>
+
+            <TransitionWrapper
+              className={s.cardName}
+              show={!hidden}
+              delay={200}
+            >
+              <span>{card.name}</span>
+              {reverse === true && <span>(reverse)</span>}
+            </TransitionWrapper>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
